e02d6f5ba253589fb2e9106dedc6eb8f56c45851
mibarra24 <79391139+mibarra24@users.noreply.github.com>
2022-04-17 08:27:36 -0700

    commit to main

---
+++ weekly_mission_1/example9/main.js
@@ -0,0 +1,22 @@
+import Pokemon from './pokemon'
+
+const Pokemon = require('./pokemon')
+
+const pikachu = new Pokemon("pikachu")
+const bulbasaur = new Pokemon("bulbasaur")
+const squirtle = new Pokemon("squirtle")
+const charmander = new Pokemon("charmander")
+
+pikachu.sayHello()
+pikachu.sayMessage("Heey!")
+
+bulbasaur.sayHello()
+bulbasaur.sayMessage("Heey!")
+
+charmander.sayHello()
+charmander.sayMessage("Heey!")
+
+squirtle.sayHello()
+squirtle.sayMessage("Heey!")
+
+console.log(Pokemon)
---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,16 @@
+export class Pokemon {
+    constructor (name) {
+        this.name = name
+      }
+
+      sayHello(){
+        console.log(`Mi pokemon ${this.name} te saluda!!!`)
+      }
+    
+      sayMessage(msg){
+        console.log(`Mi pokemon ${this.name} dice: ${msg}`)
+      }
+
+}
+
+console.log(Pokemon)
