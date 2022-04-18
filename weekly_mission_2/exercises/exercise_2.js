e7c302aeaefe150dd782e6204fea2e8a722f62ab
mibarra24 <79391139+mibarra24@users.noreply.github.com>
2022-04-18 10:00:55 -0700

    commit to main

--- weekly_mission_2/exercises/exercise_2.js
+++ weekly_mission_2/exercises/exercise_2.js
@@ -1,93 +1,97 @@
 const explorers = [
-    {
-      name: "Explorer 1",
-      exercises_completed: 10,
-      rate: 99,
-      city: "CDMX",
-      stack: [
-        "js",
-        "c#"
-      ],
-      missions: {
-        onboarding: {
-          isFinished: true,
-          exercisesFinished: true
-        },
-        frontend: {
-          isFinished: true,
-          exercisesFinished: true
-        }
-      }
-    },
-    {
-      name: "Explorer 2",
-      exercises_completed: 9,
-      city: "Veracruz",
-      rate: 50,
-      stack: [
-        "js"
-      ],
-      missions: {
-        onboarding: {
-          isFinished: false,
-          exercisesFinished: false
-        },
-        frontend: {
-          isFinished: false,
-          exercisesFinished: false
-        }
-      }
-    },
-    {
-      name: "Explorer 3",
-      exercises_completed: 3,
-      city: "Sonora",
-      rate: 100,
-      stack: [
-        "elixir"
-      ],
-      missions: {
-        onboarding: {
-          isFinished: true,
-          exercisesFinished: true
-        },
-        frontend: {
-          isFinished: false,
-          exercisesFinished: false
-        }
+  {
+    name: "Explorer 1",
+    exercises_completed: 10,
+    rate: 99,
+    city: "CDMX",
+    stack: [
+      "js",
+      "c#"
+    ],
+    missions: {
+      onboarding: {
+        isFinished: true,
+        exercisesFinished: true
+      },
+      frontend: {
+        isFinished: true,
+        exercisesFinished: true
       }
     }
-   ]
+  },
+  {
+    name: "Explorer 2",
+    exercises_completed: 9,
+    city: "Veracruz",
+    rate: 50,
+    stack: [
+      "js"
+    ],
+    missions: {
+      onboarding: {
+        isFinished: false,
+        exercisesFinished: false
+      },
+      frontend: {
+        isFinished: false,
+        exercisesFinished: false
+      }
+    }
+  },
+  {
+    name: "Explorer 3",
+    exercises_completed: 3,
+    city: "Sonora",
+    rate: 100,
+    stack: [
+      "elixir"
+    ],
+    missions: {
+      onboarding: {
+        isFinished: true,
+        exercisesFinished: true
+      },
+      frontend: {
+        isFinished: false,
+        exercisesFinished: false
+      }
+    }
+  }
+ ]
 
 console.log("Sobre esta lista, realiza lo siguiente:")
 
-console.log("Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH")
+console.log("Ejercicio 2.1: Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH")
 
 explorers.forEach(explorer => console.log(explorer.name))
 
-console.log("Imprime el stack de cada explorer, usa FOR EACH")
+console.log("Ejercicio 2.2: Imprime el stack de cada explorer, usa FOR EACH")
 
 explorers.forEach(item => console.log(item.stack))
 
-console.log("Crea una nueva lista con las listas de stacks de cada explorer, usa MAP")
+console.log("Ejercicio 2.3: Crea una nueva lista con las listas de stacks de cada explorer, usa MAP")
 
 const explorers_stacks = explorers.map(my_explorer_in_list => my_explorer_in_list.stack)
 console.log(explorers_stacks)
 
-console.log("Obtén la lista de explorers que tengan en su stack js, usa FILTER (para validar un elemento en un lista se usa el método includes)")
+console.log("Ejercicio 2.4: Obtén la lista de explorers que tengan en su stack js, usa FILTER (para validar un elemento en un lista se usa el método includes)")
 
 const explorers_with_js = explorers.filter(my_explorer_in_list => my_explorer_in_list.stack.includes("js"))
 console.log(explorers_with_js)
 
-console.log("Busca el primer explorer que sea de la CDMX, usa FIND")
+console.log("Ejercicio 2.5: Busca el primer explorer que sea de la CDMX, usa FIND")
 
-const first_explorer
+const first_explorer_in_CDMX =explorers.find(my_explorer_in_list => my_explorer_in_list.city == "CDMX")
+console.log(first_explorer_in_CDMX)
 
-console.log("Obtén la suma de todos los exercises_completed, usa REDUCE")
+console.log("Ejercicio 2.6: Obtén la suma de todos los exercises_completed, usa REDUCE")
 
-console.log("Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME")
+const all_exercises = explorers.reduce((acc, my_explorer_in_list) => acc + my_explorer_in_list.exercises_completed,0)
+console.log(all_exercises)
 
-console.log("Obtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true. Usa EVERY")
+console.log("Ejercicio 2.7: Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME")
+
+console.log("Ejercicio 2.8: Obtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true. Usa EVERY")
 
 
 
